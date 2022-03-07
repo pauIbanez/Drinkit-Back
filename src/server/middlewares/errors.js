e35b3f128269e86ac3aee402b706c849c872460b
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-07 19:43:51 +0100

    added general error handler

--- src/server/middlewares/errors.js
+++ src/server/middlewares/errors.js
@@ -1,3 +1,6 @@
+const chalk = require("chalk");
+const debug = require("debug")("drinkit:errors");
+
 const notFoundError = (req, res) => {
   const notFoundMessage = {
     error: true,
@@ -7,4 +10,21 @@ const notFoundError = (req, res) => {
   res.status(404).json(notFoundMessage);
 };
 
-module.exports = { notFoundError };
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {
+  const code = err.code || 500;
+  const message = err.send || "Internal server error";
+
+  const errorToSend = {
+    error: true,
+    code,
+    message,
+  };
+  if (!err.send) {
+    debug(chalk.redBright(`Unhandled error found!: \n ${err.message}`));
+  }
+
+  res.status(code).json(errorToSend);
+};
+
+module.exports = { notFoundError, errorHandler };
