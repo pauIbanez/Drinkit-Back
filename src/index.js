9d52ea640ae7b77843246e4777266e4554f931f3
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-07 19:32:30 +0100

    added index.js server initialization

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const debug = require("debug")("drinkit:root");
+
+const app = require("./server");
+const startServer = require("./server/startServer");
+
+const port = process.env.PORT || 4000;
+
+(async () => {
+  try {
+    await startServer(port, app);
+  } catch (error) {
+    debug(`Error: ${error.message}`);
+  }
+})();
