e02f0a1c89462c9879b626654edaa509c2f4b65b
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-07 19:28:03 +0100

    installed dependencies and added server start function

---
+++ src/server/startServer.js
@@ -0,0 +1,20 @@
+const debug = require("debug")("drinkit:server");
+
+const startServer = (port, app) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(
+        `Server listening on http://localhost${port === 80 ? "" : `:${port}`}`
+      );
+      resolve();
+    });
+
+    server.on("error", (error) => {
+      const errorMessage = `Couldn't start the server.${
+        error.code === "EADDRINUSE" ? ` Port ${port} in use` : ""
+      }`;
+      reject(new Error(errorMessage));
+    });
+  });
+
+export default startServer;
