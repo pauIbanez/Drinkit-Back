d6f3b2192a09adc61d0fd3f3cb300ba72ee78ba6
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-07 19:34:41 +0100

    added nodemon and start scripts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const debug = require("debug")("drinkit:root");
 const app = require("./server");
 const startServer = require("./server/startServer");
 
-const port = process.env.PORT || 4000;
+const port = +process.env.PORT || 4000;
 
 (async () => {
   try {
