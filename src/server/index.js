561df9d0987b4720a952e556eee44bb9e599f02c
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-07 19:45:02 +0100

    added the error middlewares to the chain

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const { default: helmet } = require("helmet");
+const { notFoundError, errorHandler } = require("./middlewares/errors");
 
 const app = express();
 
@@ -9,5 +10,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+
+app.use(notFoundError);
+app.use(errorHandler);
 
 module.exports = app;
