843a7ad07bf83cca4d8a01893bf42cf81df8c409
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-08 15:57:29 +0100

    added userData validation

---
+++ src/server/controllers/users/register.js
@@ -0,0 +1,18 @@
+const register = async (req, res, next) => {
+  const { name, lastName, email, username, password } = req.body;
+
+  if (!name || !lastName || !email || !username || !password) {
+    const invalidDataError = {
+      code: 400,
+      send: "Invalid user data",
+    };
+
+    next(invalidDataError);
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+};
+
+module.exports = register;
