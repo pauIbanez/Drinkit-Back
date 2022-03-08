a0e644a795dced95eda7503aa9aee9dda6831108
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-08 15:42:05 +0100

    configured nodemailer and added base email

---
+++ src/utils/email/mailData.js
@@ -0,0 +1,22 @@
+const getActivationEmail = (user, username, activationToken) => `
+head>
+  <style>
+    h1 {
+      font-weight: 600;
+      font-size: 40px;
+    }
+  </style>
+</head>
+<div>
+  <h1>Welcome to We Robot</h1>
+  <p>Hello ${user},</p>
+  <p>
+    Thank you for joining We Robot. Please visit the link bellow to activate
+    your user: ${username}.
+  </p>
+  <a href="https://robots-pau-ibanez.netlify.app/users/activate/${activationToken}">Click here to activate ${username}</a>
+</div>
+`;
+
+const activationSubject = "Drinkit account activation";
+module.exports = { getActivationEmail, activationSubject };
