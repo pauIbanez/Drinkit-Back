e42833cc5b298fe1f68a4b8d5d2898c3d944f3a9
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-08 21:27:41 +0100

    added test to register case: repeated username

--- src/server/controllers/users/register.test.js
+++ src/server/controllers/users/register.test.js
@@ -1,9 +1,15 @@
+const User = require("../../../database/models/User");
 const register = require("./register");
 
 describe("Given register controller", () => {
+  const name = "nein";
+  const lastName = "lasname";
+  const email = "imail";
+  const username = "usernaim";
+  const password = "passguord";
+
   describe("When it's passed a req with invalid user data and a next", () => {
     test("Then it should invoke next with the error 'Invalid user data' and the code 400", async () => {
-      const name = "sadasd";
       const req = {
         body: {
           name,
@@ -13,6 +19,37 @@ describe("Given register controller", () => {
       const expectedError = {
         code: 400,
         send: "Invalid user data",
+      };
+
+      const next = jest.fn();
+
+      await register(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+
+  describe("When it's passed a req with a valid user but username is repeated", () => {
+    test("Then it should invoke next with the error 'username' and code 409", async () => {
+      const req = {
+        body: {
+          name,
+          lastName,
+          email,
+          username,
+          password,
+        },
+      };
+
+      User.create = jest
+        .fn()
+        .mockRejectedValue(
+          new Error("Path `username` is marked as unique o noseque")
+        );
+
+      const expectedError = {
+        code: 409,
+        send: "username",
       };
 
       const next = jest.fn();
