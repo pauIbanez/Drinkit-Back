126a0769b8d82f9ce9b53a72b3c596251d8b12a8
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-10 16:34:46 +0100

    added seccond test for userValidator

--- src/server/middlewares/joiValidators/userValidator/userValidator.test.js
+++ src/server/middlewares/joiValidators/userValidator/userValidator.test.js
@@ -19,4 +19,28 @@ describe("Given userValidator", () => {
       expect(next).toHaveBeenCalledWith();
     });
   });
+
+  describe("When instanciated passing a req with a user with an invalid password, a res and a next", () => {
+    test("Then it should call next with nothing", () => {
+      const req = {
+        body: {
+          name: "pablo",
+          lastName: "sadaasd",
+          email: "asdasdasdaa@dfsda.com",
+          username: "asadadsf",
+          password: "s",
+        },
+      };
+      const next = jest.fn();
+
+      const expectedError = expect.objectContaining({
+        code: 400,
+        send: expect.stringContaining("password"),
+      });
+
+      userValidator(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
 });
