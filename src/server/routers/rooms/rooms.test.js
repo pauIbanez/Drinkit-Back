1f11c275fc76ccc7aa1c209ec58da688c9964459
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-10 18:43:39 +0100

    added first test for room listing

---
+++ src/server/routers/rooms/rooms.test.js
@@ -0,0 +1,68 @@
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const { default: mongoose } = require("mongoose");
+const request = require("supertest");
+const app = require("../..");
+
+const connectToDB = require("../../../database");
+const Game = require("../../../database/models/Game");
+const Room = require("../../../database/models/Room");
+const User = require("../../../database/models/User");
+const generateUser = require("../../../utils/users/creation/generateUser");
+
+const name = "nein";
+const lastName = "lasname";
+const email = "someemail@some.email";
+const username = "usernaim";
+const password = "passguord";
+
+let mongoServer;
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const connectionString = mongoServer.getUri();
+
+  await connectToDB(connectionString);
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
+
+beforeEach(async () => {
+  User.create({
+    ...generateUser(name, lastName, email, username, password),
+    _id: "622a0b309b056758689f06e9",
+  });
+  Game.create({
+    name: "Somegame",
+    drunkness: "high",
+    duration: 30,
+    minPlayers: 4,
+    maxPlayers: 12,
+    gameInfo: {
+      setup: "ahahhaa",
+      howToPlay: "you don't",
+    },
+    _id: "6229dc199b056758689f06e6",
+  });
+  Room.create({
+    leader: "622a0b309b056758689f06e9",
+    game: "6229dc199b056758689f06e6",
+  });
+});
+
+afterEach(async () => {
+  await User.deleteMany({});
+  await Room.deleteMany({});
+  await Game.deleteMany({});
+});
+
+describe("Given /rooms/list endpoint", () => {
+  describe("When it recieves a request ", () => {
+    test("Then it should return a list of all rooms with status 200", async () => {
+      const body = await request(app).get("/rooms/list").expect(200);
+
+      expect(body.rooms).toHaveLength(1);
+    });
+  });
+});
