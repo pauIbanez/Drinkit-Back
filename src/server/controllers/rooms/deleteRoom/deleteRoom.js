fbbfc9570a956c9d3ba50e7849fd286fba5bb213
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-13 12:39:53 +0100

    added deleteRoom controller

---
+++ src/server/controllers/rooms/deleteRoom/deleteRoom.js
@@ -0,0 +1,22 @@
+const Room = require("../../../../database/models/Room");
+
+const deleteRoom = async (req, res, next) => {
+  const { user } = req;
+  const playerRoom = await Room.findOne({ leader: user.id });
+
+  if (!playerRoom) {
+    const error = {
+      code: 404,
+      send: "No rooms found",
+    };
+    next(error);
+    return;
+  }
+
+  try {
+    await Room.findByIdAndDelete(playerRoom.id);
+    res.json({});
+  } catch (error) {
+    next(error);
+  }
+};
