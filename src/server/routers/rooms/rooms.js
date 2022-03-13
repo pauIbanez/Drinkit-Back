38d2559df48d815a35056244ddd520652959183d
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-13 12:52:45 +0100

    added the deleteRoom to the router chain

--- src/server/routers/rooms/rooms.js
+++ src/server/routers/rooms/rooms.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const createRoom = require("../../controllers/rooms/createRoom/createRoom");
+const deleteRoom = require("../../controllers/rooms/deleteRoom/deleteRoom");
 const listRooms = require("../../controllers/rooms/listRooms/listRooms");
 const roomValidator = require("../../middlewares/joiValidators/roomValidator/roomValidator");
 
@@ -7,5 +8,6 @@ const router = express.Router();
 
 router.get("/list", listRooms);
 router.post("/create", roomValidator, createRoom);
+router.delete("/delete", deleteRoom);
 
 module.exports = router;
