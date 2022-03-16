5354ac6c4994d14d1e3b907541aeb7484af7000b
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-16 17:49:45 +0100

    added test for invalid password

--- src/server/controllers/users/login/login.test.js
+++ src/server/controllers/users/login/login.test.js
@@ -60,4 +60,36 @@ describe("Given login", () => {
       expect(next).toHaveBeenCalledWith(expectedError);
     });
   });
+
+  describe("When it's passed an invalid password", () => {
+    test("Then it should call next with an error with code 401 and message 'Incorrect username or password'", async () => {
+      const req = {
+        body: {
+          username: "username",
+          password: "password",
+        },
+      };
+
+      const next = jest.fn();
+
+      const expectedError = {
+        code: 401,
+        send: "Incorrect username or password",
+      };
+
+      const foundUser = {
+        id: "oyeah",
+        credentials: {
+          password: "somepassword",
+        },
+      };
+
+      User.findOne = jest.fn().mockResolvedValue(foundUser);
+      bcrypt.compare = jest.fn().mockReturnValue(false);
+
+      await login(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
 });
