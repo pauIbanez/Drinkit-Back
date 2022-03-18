eaf48b7bd224afa0af8c4e28d83b651b20fe9024
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-18 16:12:39 +0100

    added test for user not found

--- src/server/controllers/users/sendData/sendData.test.js
+++ src/server/controllers/users/sendData/sendData.test.js
@@ -32,4 +32,27 @@ describe("Given sendData", () => {
       expect(res.json).toHaveBeenCalledWith(userData);
     });
   });
+
+  describe("When it's instanciated passing a req with a user and the user is not found", () => {
+    test("Then it should call next with an error with code 404 and msg 'USer not found'", async () => {
+      const req = {
+        user: {
+          id: "userId",
+        },
+      };
+
+      const next = jest.fn();
+
+      const expectedError = {
+        code: 404,
+        send: "User not found",
+      };
+
+      User.findById = jest.fn().mockResolvedValue(null);
+
+      await sendData(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
 });
