8c45e29b959255398559802c3af7397af3dff2ec
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-19 16:39:15 +0100

    added static resources to server pointing to public

--- src/server/index.js
+++ src/server/index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/rooms", roomsRouter);
 app.use("/accounts", usersRouter);
