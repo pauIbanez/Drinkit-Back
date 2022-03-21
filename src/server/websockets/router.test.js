70561f2ca9435ee40d1906ebce449da1f7895d68
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-22 00:01:59 +0100

    added test for lobby path with correct user

--- src/server/websockets/router.test.js
+++ src/server/websockets/router.test.js
@@ -1,16 +1,57 @@
+const {
+  connectedUsers,
+} = require("../../database/liveDatabase/userLiveDatabase/userLiveDatabase");
+const User = require("../../database/models/User");
 const connectionRouter = require("./connectionRouter/connectionRouter");
+const lobbyRouter = require("./lobbyRouter/lobbyRouter");
 const router = require("./router");
 
 jest.mock("./connectionRouter/connectionRouter");
+jest.mock("./lobbyRouter/lobbyRouter");
 
 describe("Given router", () => {
   describe("When it's intanciated passing a message without reason", () => {
-    test("When it should call connectionRouter", () => {
+    test("Then it should call connectionRouter", () => {
       const message = {};
       const connection = {};
       router(message, connection);
 
       expect(connectionRouter).toHaveBeenCalledWith(message, connection);
+    });
+  });
+
+  describe("When it's intanciated passing a message with the reason as 'lobby'", () => {
+    beforeEach(() => {
+      connectedUsers.push({
+        id: "playerId",
+        profile: {},
+        connection: {},
+      });
+    });
+    test("Then it should call lobbyRouter", async () => {
+      const message = {
+        reason: "lobby",
+      };
+      const connection = {};
+      const expectedPlayer = {
+        id: "playerId",
+        profile: {},
+        connection: {},
+      };
+
+      const player = {
+        id: "playerId",
+        profile: {},
+      };
+      User.findById = jest.fn().mockResolvedValue(player);
+
+      await router(message, connection);
+
+      expect(lobbyRouter).toHaveBeenCalledWith(
+        message,
+        connection,
+        expectedPlayer
+      );
     });
   });
 });
