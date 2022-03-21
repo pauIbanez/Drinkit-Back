506f3d8270a614f0ad12c24d1bc25f048eb6512c
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-21 19:24:53 +0100

    checked if player has a connection before chain

--- src/server/websockets/router.js
+++ src/server/websockets/router.js
@@ -3,17 +3,20 @@ const connectionRouter = require("./connectionRouter/connectionRouter");
 const lobbyRouter = require("./lobbyRouter/lobbyRouter");
 
 const router = async (message, connection) => {
-  const foundUser = await User.findById(message.userId);
-  if (!foundUser) {
-    connection.send(
-      JSON.stringify({
-        error: true,
-        message: "Invalid player",
-      })
-    );
+  let foundUser;
+  let player;
+  if (message.reason) {
+    foundUser = await User.findById(message.userId);
+    if (!foundUser) {
+      connection.send(
+        JSON.stringify({
+          error: true,
+          message: "Invalid player",
+        })
+      );
+    }
+    player = { id: foundUser.id, profile: foundUser.profile, connection };
   }
-
-  const player = { id: foundUser.id, profile: foundUser.profile, connection };
 
   switch (message.reason) {
     case "lobby":
