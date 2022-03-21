7148862ac74f515b6703c1210334937519032537
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-21 19:03:36 +0100

    removed test greet

--- src/server/websockets/lobbyRouter/piramideLobbyRouter/piramideLobbyRouter.js
+++ src/server/websockets/lobbyRouter/piramideLobbyRouter/piramideLobbyRouter.js
@@ -7,9 +7,6 @@ const piramideLobbyRouter = async (
   // eslint-disable-next-line no-param-reassign
   player.connection = connection;
 
-  player.connection.send(
-    JSON.stringify(`Welcome ${player.profile.username} from player`)
-  );
   switch (message.type) {
     case "join":
       lobbyInstance.lobby.appendPlayer(player);
