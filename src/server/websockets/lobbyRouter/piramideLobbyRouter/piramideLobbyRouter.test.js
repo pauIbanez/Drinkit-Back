8c27f00e051de9c4d7691fd2d654c6a449fa26c4
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-21 23:37:48 +0100

    added test for default case

--- src/server/websockets/lobbyRouter/piramideLobbyRouter/piramideLobbyRouter.test.js
+++ src/server/websockets/lobbyRouter/piramideLobbyRouter/piramideLobbyRouter.test.js
@@ -9,6 +9,26 @@ describe("Given piramideLobbyRouter", () => {
       id: "userId",
     });
   });
+  describe("When it's passed a messagewithout type", () => {
+    test("Then it should so nothing", () => {
+      const message = {};
+
+      const lobbyInstance = {
+        lobby: {
+          appendPlayer: jest.fn(),
+        },
+      };
+
+      const player = {
+        id: "userId",
+      };
+
+      piramideLobbyRouter(message, lobbyInstance, player);
+
+      expect(lobbyInstance.lobby.appendPlayer).not.toHaveBeenCalled();
+    });
+  });
+
   describe("When it's passed a message with type join and a lobby", () => {
     test("Then it should call the lobby's append player", () => {
       const message = {
