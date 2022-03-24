7e357779fb99f05d52dcc413ecf6b31eb2810b5f
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-24 06:17:11 +0100

    added test for setupLobby

--- src/database/liveDatabase/lobbiesLiveDatabase/piramideLobby/PiramideLobby.test.js
+++ src/database/liveDatabase/lobbiesLiveDatabase/piramideLobby/PiramideLobby.test.js
@@ -47,4 +47,45 @@ describe("Given PiramideLobby constructor", () => {
       expect(piramideLobby).toEqual(expectedLobby);
     });
   });
+
+  describe("When it's instanciated passing a lobbyConfig, a leader and a reference but lobby already set up", () => {
+    test("Then it should throw an error", () => {
+      const lobbyConfig = {
+        twoDecks: false,
+        jokers: false,
+        leftovers: false,
+        modifiers: [],
+      };
+      const reference = {
+        id: "id",
+        sharedId: "sharedId",
+      };
+
+      const leader = {
+        id: "leaderId",
+        profile: {
+          username: "leader",
+        },
+        connection: {
+          send: jest.fn(),
+        },
+      };
+
+      const expectedMessage = "Lobby is already set up!";
+
+      const piramideLobby = new PiramideLobby(lobbyConfig, leader, reference);
+
+      const mockHandler = jest.fn();
+
+      try {
+        piramideLobby.setupLobby(lobbyConfig);
+      } catch (error) {
+        mockHandler(error);
+      }
+
+      expect(mockHandler).toHaveBeenCalledWith(
+        expect.objectContaining({ message: expectedMessage })
+      );
+    });
+  });
 });
